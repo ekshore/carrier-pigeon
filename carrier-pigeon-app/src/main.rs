a4ef47ecdda777e47c46dd7956b480a4281ea83a
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2024-04-18 16:20:23 -0500

    More Iced clean up

--- carrier-pigeon-app/src/main.rs
+++ carrier-pigeon-app/src/main.rs
@@ -9,27 +9,9 @@ use carrier_pigeon_lib::PigeonError;
 
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
-#[derive(Debug, Clone)]
-enum Message {
-    Loading,
-    SelectCollection,
-    ViewCollection(Option<String>),
-}
-
-#[derive(Debug, Default)]
-enum App {
-    #[default]
-    Loading,
-    SelectCollection(Option<Vec<String>>),
-    ViewCollection(Option<String>),
-}
-
 #[tokio::main]
 async fn main() -> Result<(), PigeonError> {
     let config = simplelog::ConfigBuilder::new()
-        .add_filter_ignore_str("wgpu")
-        .add_filter_ignore_str("iced_wgpu")
-        .add_filter_ignore_str("naga")
         .build();
     let _logger = CombinedLogger::init(vec![TermLogger::new(
         LevelFilter::Debug,
