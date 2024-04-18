3d32696f3f5858e78a3d726a122a8ab9b9139767
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2024-04-18 16:17:54 -0500

    Removing iced.
    I have decided that for the time being I'm developing this application
    as a tui application instead of a gui.

--- carrier-pigeon-app/src/main.rs
+++ carrier-pigeon-app/src/main.rs
@@ -1,17 +1,11 @@
 #[allow(unused_imports)]
 use log::{debug, error, info, warn};
 
-use iced::widget::{self, column, container};
-use iced::Application;
-use iced::{Command, Element};
-
 #[allow(unused_imports)]
 use reqwest::header::HeaderMap;
 use simplelog::{ColorChoice, CombinedLogger, LevelFilter, TermLogger, TerminalMode};
 
-use carrier_pigeon_lib::Collection;
-#[allow(unused_imports)]
-use carrier_pigeon_lib::{Header, PigeonError, Request};
+use carrier_pigeon_lib::PigeonError;
 
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
@@ -30,79 +24,6 @@ enum App {
     ViewCollection(Option<String>),
 }
 
-impl Application for App {
-    type Executor = iced::executor::Default;
-    type Message = Message;
-    type Theme = iced::Theme;
-    type Flags = ();
-
-    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
-        (App::default(), Command::none())
-    }
-
-    fn title(&self) -> String {
-        "Carrier Pigeon".into()
-    }
-
-    fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
-        match message {
-            Message::Loading => *self = Self::Loading,
-            Message::SelectCollection => {
-                *self =
-                    Self::SelectCollection(Some(vec!["Does".into(), "This".into(), "Work".into()]))
-            }
-            Message::ViewCollection(c) => *self = Self::ViewCollection(c),
-        }
-        Command::none()
-    }
-
-    fn view(&self) -> Element<'_, Self::Message, Self::Theme, iced::Renderer> {
-        match self {
-            Self::Loading => test_component("Loading".into()),
-            Self::SelectCollection(Some(collections)) => {
-                let requests = request_list(collections);
-                let load_btn = widget::button("Load").on_press(Message::Loading);
-                let requests = requests.push(load_btn);
-                container(requests).into()
-            },
-            Self::SelectCollection(None) => test_component("SelectCollection".into()),
-            Self::ViewCollection(_collection) => test_component(_collection.as_ref().unwrap().into()),
-        }
-    }
-
-    fn theme(&self) -> Self::Theme {
-        Self::Theme::GruvboxDark
-    }
-}
-
-fn request_list(
-    request_names: &[String],
-) -> widget::Column<'_, Message, iced::Theme, iced::Renderer> {
-    let request_buttons: Vec<Element<Message, iced::Theme, iced::Renderer>> = request_names
-        .iter()
-        .map(|name| {
-            let content = column![
-                widget::text(name),
-                widget::Rule::horizontal(5),
-            ];
-            widget::Button::new(content)
-                .width(100)
-                .on_press(Message::ViewCollection(Some(name.clone()))).into()
-        })
-        .collect();
-    column(request_buttons)
-}
-
-fn test_component(display_text: String) -> Element<'static, Message, iced::Theme, iced::Renderer> {
-    container(column![
-        widget::text(display_text),
-        widget::button("Loading").on_press(Message::Loading),
-        widget::button("Select Collection").on_press(Message::SelectCollection),
-        widget::button("View Collection").on_press(Message::ViewCollection(None)),
-    ])
-    .into()
-}
-
 #[tokio::main]
 async fn main() -> Result<(), PigeonError> {
     let config = simplelog::ConfigBuilder::new()
@@ -116,8 +37,6 @@ async fn main() -> Result<(), PigeonError> {
         TerminalMode::Mixed,
         ColorChoice::Auto,
     )]);
-
-    App::run(iced::Settings::default())?;
 
     Ok(())
 }
