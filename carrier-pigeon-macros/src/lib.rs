80e58e87400ea8ca255766b48458fcc928983d74
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2024-10-18 23:06:46 -0500

    MACROS!!!
    Added a bunch of macros to make working with tabs easier.
    
    Fixed Macros
    
    Fixed the macros so they play nicely with attributes.

--- carrier-pigeon-macros/src/lib.rs
+++ carrier-pigeon-macros/src/lib.rs
@@ -2,6 +2,101 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput, Fields, Type, Variant};
 
+#[proc_macro_derive(DisplayEnum)]
+pub fn display_enum(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let options: Vec<proc_macro2::TokenStream> = if let syn::Data::Enum(data) = input.data {
+        data.variants
+            .into_iter()
+            .map(|v| v.ident)
+            .map(|variant| quote! { #name::#variant => stringify!(#variant), })
+            .collect()
+    } else {
+        panic!("DisplayEnum only supports Enums");
+    };
+
+    TokenStream::from(quote! {
+        impl std::fmt::Display for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(
+                    f,
+                    "{}",
+                    match self {
+                        #(#options)*
+                    }
+                )
+            }
+        }
+    })
+}
+
+#[proc_macro_derive(ListEnum)]
+pub fn list_enum(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+    let entries: Vec<proc_macro2::TokenStream> = if let syn::Data::Enum(data) = input.data {
+        data.variants
+            .into_iter()
+            .map(|v| v.ident)
+            .map(|val| quote! { #name::#val, })
+            .collect()
+    } else {
+        panic!("ListEnum only works for Enums.");
+    };
+
+    TokenStream::from(quote! {
+        impl #name {
+            pub fn to_vec() -> Vec<Self> {
+                vec![ #(#entries)* ]
+            }
+        }
+    })
+}
+
+#[proc_macro_derive(OrderedEnum)]
+pub fn ordered_enum(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+    let (to_usize, to_varient) = if let syn::Data::Enum(data) = input.data {
+        data.variants.into_iter()
+            .map(|v| v.ident)
+            .enumerate().fold(
+            (vec![], vec![]),
+            |(mut to_usize, mut to_varient), (idx, varient)| {
+                to_usize.push(quote! { #name::#varient => #idx, });
+                to_varient.push(quote! { #idx => #name::#varient, });
+                (to_usize, to_varient)
+            },
+        )
+    } else {
+        panic!("OrderedEnum only supports enums.");
+    };
+
+    let expanded = quote! {
+        impl From<#name> for usize {
+            fn from(val: #name) -> Self {
+                match val {
+                    #(#to_usize)*
+                }
+            }
+        }
+
+        impl From<usize> for #name {
+            fn from(val: usize) -> Self {
+                match val {
+                    #(#to_varient)*
+                    _ => panic!("Index out of bounds")
+                }
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
 #[proc_macro_derive(FromWrappedError, attributes(wrapper))]
 pub fn from_wrapped_error(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -55,7 +150,7 @@ pub fn from_wrapped_error(input: TokenStream) -> TokenStream {
         panic!("Only enums error types are supported at this time");
     };
 
-    let expanded = quote!{
+    let expanded = quote! {
         #(#from_implimentations)*
     };
     TokenStream::from(expanded)
