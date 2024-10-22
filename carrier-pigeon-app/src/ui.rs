3f6f5c77f917a858edf93d9efe0721bb414280a0
Caleb Ekstrand <ekstrand.caleb@ekshore.net>
2024-10-21 19:57:46 -0500

    formatting function

--- carrier-pigeon-app/src/ui.rs
+++ carrier-pigeon-app/src/ui.rs
@@ -278,6 +278,20 @@ pub fn draw(app: &mut App, frame: &mut Frame) {
     }
 }
 
+fn convert_case(str: String) -> String {
+    let bytes = str.as_bytes().to_owned();
+    let mut return_bytes: Vec<u8> = Vec::with_capacity(bytes.len());
+    return_bytes.push(bytes[0]);
+
+    for byte in bytes.iter().skip(1) {
+        if *byte < b'a' {
+            return_bytes.push(b' ');
+        }
+        return_bytes.push(*byte);
+    }
+    String::from_utf8(return_bytes).expect("If the blows up we have bigger problems")
+}
+
 use carrier_pigeon_core::{Method, Request};
 fn request_text(req: &Request) -> Text<'_> {
     let method_style = match req.method {
@@ -292,8 +306,19 @@ fn request_text(req: &Request) -> Text<'_> {
     .into()
 }
 
+#[cfg(test)]
+pub mod test {
+    use super::*;
 
+    #[test]
+    fn convert_case_conversion() {
+        let converted_string = convert_case("HelloWorld".into());
+        assert_eq!("Hello World", converted_string);
     }
 
+    #[test]
+    fn convert_case_no_conversion() {
+        let converted_string = convert_case("Hello".into());
+        assert_eq!("Hello", converted_string);
     }
 }
